636481f3059fb52b28773f46ec8b3817a8a5ced3
Sven <imljh@hotmail.com>
2019-06-18 16:13:46 +0800

    fix carelessness

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-exports.default = {
+module.exports = {
     entry: {
         app: './src/app.js'
     },
